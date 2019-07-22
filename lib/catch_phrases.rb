77eeb28bf55e539845fdb4d6daa87e3f47b94c01
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-07-22 02:43:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,7 @@
 mario puts out 'It's-a me, Mario!''
 toadstool puts out 'Thank You Mario! But Our Princess  Is In Another Castle!
 link puts out 'Its's Dangerous To Go Alone! Take This.'
-
+any_phrase takes in an argument and puts out the catch phrase
 def toadstool
   puts status
 end
